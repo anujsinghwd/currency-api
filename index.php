4f1494dba8d8ba8aa014be6c1718b2ca15d06826
anujsinghwd <anuj_singh@outlook.in>
2019-10-16 18:26:31 +0530

    base added

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+header('Content-Type: application/json');
 $url = "https://www.exchange-rates.org/converter/USD/INR/1/Y";
 $html = file_get_contents($url);
 $currency = new DOMDocument();
 if (!empty($html)) {
     $currency->loadHTML($html);
-    libxml_clear_errors(); 
+    libxml_clear_errors();
     $currency_xpath = new DOMXPath($currency);
 
     $conversion = $currency_xpath->query('//span[@id="ctl00_M_lblConversion"]');
@@ -12,13 +13,18 @@ if (!empty($html)) {
 
     foreach($conversion as $node)
     {
-        echo $node->nodeValue;
+        $conv = $node->nodeValue;
+        $ex = explode("=", $conv);
+        print_r($ex);
     }
 
     foreach($inverse as $node)
     {
-        echo $node->nodeValue;
+        // echo $node->nodeValue;
     }
+
+    $res_arr = array('base' => 'USD' ,'date' => date("Y-m-d"));
+    echo json_encode($res_arr);
 }
 
 
