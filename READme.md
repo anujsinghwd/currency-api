ffe279afe58a7019abf9cc81c026d604bb0a6427
anujsinghwd <anuj_singh@outlook.in>
2019-10-17 13:11:00 +0530

    docs updated

---
+++ READme.md
@@ -0,0 +1,16 @@
+# currency_API
+
+# API Usage
+
+  - features
+    - currency conversion
+
+
+
+
+# Contributing
+
+- Data Fetch From [Exchange-Rates](https://www.exchange-rates.org) unofficialy.
+
+- If the API is not working properly, please open a issue.
+        
--- index.php
+++ index.php
@@ -1,31 +1,74 @@
 <?php
 header('Content-Type: application/json');
-$url = "https://www.exchange-rates.org/converter/USD/INR/1/Y";
+$to =$_REQUEST['to'];
+$from =$_REQUEST['from'];
+$amt = isset($_REQUEST['amt']) ? $_REQUEST['amt'] : 1;
+$response;
+if(isset($to) && isset($from)){
+$url = "https://www.exchange-rates.org/converter/".$from."/".$to."/".$amt."/Y";
+
+/**
+ * @api {get} currency?to=USD&from=INR&amt=10 Request Currency information
+ * @apiName GetCurrency Info
+ * @apiGroup Currency
+ *
+ * @apiParam {from} from Currency Code.
+ * @apiParam {to} to Currency Code.
+ *
+ */
+
 $html = file_get_contents($url);
 $currency = new DOMDocument();
 if (!empty($html)) {
-    $currency->loadHTML($html);
-    libxml_clear_errors();
-    $currency_xpath = new DOMXPath($currency);
+        $currency->loadHTML($html);
+        libxml_clear_errors();
+        $currency_xpath = new DOMXPath($currency);
 
-    $conversion = $currency_xpath->query('//span[@id="ctl00_M_lblConversion"]');
-    $inverse = $currency_xpath->query('//span[@id="ctl00_M_lblInverseConvertion"]');
+        $conversion = $currency_xpath->query('//span[@id="ctl00_M_lblConversion"]');
+        $inverse = $currency_xpath->query('//span[@id="ctl00_M_lblInverseConvertion"]');
+        $FromAmount = $currency_xpath->query('//span[@id="ctl00_M_lblFromAmount"]');
+        $ToAmount = $currency_xpath->query('//span[@id="ctl00_M_lblToAmount"]');
+        $converted_data = [];
+        $innverted_data = [];
+        $total_amt = [];
+        foreach($conversion as $node)
+        {
+            $conv = $node->nodeValue;
+            $ex = explode("=", $conv);
+            $converted_data["base"] = $from;
+            $converted_data["currency"] = $to;
+            $converted_data["numeric"] = (float)substr($ex[1], 0, (strpos($ex[1], "INR")-1));
+        }
 
-    foreach($conversion as $node)
-    {
-        $conv = $node->nodeValue;
-        $ex = explode("=", $conv);
-        print_r($ex);
+        foreach($inverse as $node)
+        {
+            $conv = $node->nodeValue;
+            $ex = explode("=", $conv);
+            $innverted_data["base"] = $to;
+            $innverted_data["currency"] = $from;
+            $innverted_data["numeric"] = (float)substr($ex[1], 0, (strpos($ex[1], "INR")-1));
+        }
+
+        foreach($FromAmount as $node)
+        {
+            $conv = $node->nodeValue;
+            $total_amt[$from] = array("amt" => $conv);
+        }
+
+        foreach($ToAmount as $node)
+        {
+            $conv = $node->nodeValue;
+            $total_amt[$to] = array("amt" => $conv);
+        }
+
+        $res_arr = array('date' => date("Y-m-d"), "unit_converted_data" => $converted_data, "unit_inverted_data" => $innverted_data, "total" => $total_amt);
+        $response = json_encode($res_arr);
     }
-
-    foreach($inverse as $node)
-    {
-        // echo $node->nodeValue;
-    }
-
-    $res_arr = array('base' => 'USD' ,'date' => date("Y-m-d"));
-    echo json_encode($res_arr);
 }
-
+else{
+    header('HTTP/1.0 400 Bad Request'); 
+    $response = json_encode(array());
+}
+echo $response;
 
 ?>
